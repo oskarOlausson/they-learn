9e6b87eca76b428ef30222f1e5fffdd1163eb870
oskarOlausson <kullenoskar@gmail.com>
2016-10-30 13:08:54 +0100

    wobbly corpses

--- src/corpse.js
+++ src/corpse.js
@@ -1,22 +1,61 @@
+"use strict";
 
 var Corpse = Class({
 	constructor: function constructor(x, y) {
+		/*
 		this.sprite = new PIXI.Sprite(PIXI.loader.resources['corpse'].texture);
     	this.sprite.x = x;
     	this.sprite.y = y;
 		this.sprite.anchor.set(.5, .5);
-		this.sprite.rotation = (Math.PI / 180) * ((Math.random() * 20) -10);
 
 		//furthest back (except background)
 		OBJECTS.addChildAt(this.sprite, 0);
+		*/
 
 		this.dead = false;
 		this.timerMax = 60 * 5;
 		this.timer = this.timerMax;
+
+		// build a rope!
+		var texture = PIXI.loader.resources['corpse'].texture;
+		var number = 5;
+		var height = 130;
+		this.ropeLength = height / (number + 5);
+
+		this.points = [];
+
+		for (var i = 0; i < number; i++) {
+		    this.points.push(new PIXI.Point(i * this.ropeLength, 0));
+		}
+
+		this.strip = new PIXI.mesh.Rope(texture, this.points);
+		this.count = 0;
+
+		this.strip.x = x;
+		this.strip.y = y;
+
+		OBJECTS.addChildAt(this.strip, 0);
+
 	},
 
 	update: function update() {
-		this.sprite.alpha = 0.5 * this.timer / this.timerMax;
+
+		this.count += 0.05;
+
+	    // make the wobble
+	    for (var i = 0; i < this.points.length; i++) {
+
+	        
+	        this.points[i].x = i * this.ropeLength + Math.cos((i * 0.3) + this.count) * 20;
+	        this.points[i].y = Math.sin((i * 0.5) + this.count) * 30;
+    
+
+	    }
+	  
+	  	if (this.count >= Math.PI * 2) this.count -= Math.PI * 2;
+
+
+		this.strip.alpha = 0.5 * this.timer / this.timerMax;
 		this.timer -= 1;
 		if (this.timer <= 0) {
 			this.dead = true;
@@ -24,7 +63,7 @@ var Corpse = Class({
 	},
 
 	remove: function remove() {
-		OBJECTS.removeChild(this.sprite);
+		OBJECTS.removeChild(this.strip);
 	},
 
 	getIfDead: function getIfDead() {
